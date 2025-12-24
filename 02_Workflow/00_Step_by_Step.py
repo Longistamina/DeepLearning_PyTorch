4d08445122837a857692171698245a6dbcf54248
LongSNU <longdpt1002@snu.ac.kr>
2025-12-24 16:11:33 +0900

    added contents

---
+++ 02_Workflow/00_Step_by_Step.py
@@ -0,0 +1,9 @@
+'''
+1. Data preparation and loading
+2. Build model
+3. Fitting the model to data (training)
+4. Evaluation
+5. Making predictions (inference)
+6. Saving and Loading a model
+7. Put everything together
+'''
