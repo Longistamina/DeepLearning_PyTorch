ae4717adb8f79157eda7b87924376029b3c529a7
LongSNU <longdpt1002@snu.ac.kr>
2026-02-04 16:32:20 +0900

    updated contents

--- 07_Transformer_SelfAttention/00_Videos_to_watch.py
+++ 07_Transformer_SelfAttention/00_Videos_to_watch.py
@@ -2,6 +2,7 @@
 Word2Vec embedding: https://www.youtube.com/watch?v=viZrOnJclY0&list=PLblh5JKOoLUIxGDQs4LFFD--41Vzf-ME1&index=17
 Seq2Seq translator: https://www.youtube.com/watch?v=viZrOnJclY0&list=PLblh5JKOoLUIxGDQs4LFFD--41Vzf-ME1&index=17
 Attention mechanism: https://www.youtube.com/watch?v=PSs6nxngL6k&list=PLblh5JKOoLUIxGDQs4LFFD--41Vzf-ME1&index=19
+Visualize the Attention: https://www.youtube.com/watch?v=RNF0FvRjGZk
 Transformer: https://www.youtube.com/watch?v=zxQyTK8quyY&list=PLblh5JKOoLUIxGDQs4LFFD--41Vzf-ME1&index=20
 
 Transformer decoder: https://www.youtube.com/watch?v=bQ5BoolX9Ag&list=PLblh5JKOoLUIxGDQs4LFFD--41Vzf-ME1&index=21
